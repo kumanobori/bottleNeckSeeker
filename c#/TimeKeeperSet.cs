1fc826fec8f42aa04dba57ec8751727496fc89c8
kumanobori <jirosv01@gmail.com>
2019-06-02 22:27:37 +0900

    initial commit

---
+++ c#/TimeKeeperSet.cs
@@ -0,0 +1,151 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace TimeKeeperSet {
+
+    /// <summary>
+    /// 時間計測用クラスをまとめて扱うクラス
+    /// </summary>
+    class TimeKeeperManager {
+        string title;
+        private SortedDictionary<double, TimeKeeper> timeKeepers = new SortedDictionary<double, TimeKeeper>();
+
+        /// <summary>
+        /// 表題なしでインスタンスを生成する
+        /// </summary>
+        public TimeKeeperManager() : this(""){} 
+        /// <summary>
+        /// 表題とともにインスタンスを生成する
+        /// </summary>
+        /// <param name="title">インスタンスにつける表題</param>
+        public TimeKeeperManager(String title) {
+            this.title = title;
+        }
+
+        /// <summary>
+        /// 計測開始する
+        /// </summary>
+        /// <param name="id">時計計測クラスの識別子</param>
+        public void start(double id) {
+            this.start (id, "");
+        }
+
+        /// <summary>
+        /// 表題指定つきで計測開始する
+        /// </summary>
+        /// <param name="id">時計計測クラスの識別子</param>
+        /// <param name="title">時計計測クラスの表題</param>
+        public void start (double id, string title) {
+            if (!timeKeepers.ContainsKey(id)) {
+                timeKeepers.Add(id, new TimeKeeper(title));
+            } else {
+                if (!timeKeepers[id].title.Equals(title)) {
+                    timeKeepers[id].isTitleChanged = true;
+                }
+            }
+            timeKeepers[id].start();
+        }
+
+        /// <summary>
+        /// idを指定して計測停止する
+        /// </summary>
+        /// <param name="id">時計計測クラスの識別子</param>
+        public void stop(double id) {
+            timeKeepers[id].stop();
+        }
+
+        /// <summary>
+        /// 計測対象すべての結果を出力する
+        /// </summary>
+        /// <returns>計測結果を説明する文字列</returns>
+        public string report() {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("------------------------------------\n");
+            sb.Append(string.Format("Timekeepers {0} report\n", title));
+            foreach(var(key, val) in timeKeepers) {
+                sb.Append(key + ": " + val.report() + "\n");
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 時間計測用クラス
+        /// </summary>
+        class TimeKeeper {
+            /// <summary>
+            /// このインスタンスの表題
+            /// </summary>
+            public String title {get;} = "";
+            /// <summary>
+            /// 表題が変えられた場合にtrueとなるフラグ
+            /// </summary>
+            public bool isTitleChanged {get;set;} = false;
+
+            /// <summary>
+            /// startした回数
+            /// </summary>
+            private int countStart = 0;
+            /// <summary>
+            /// stopした回数
+            /// </summary>
+            private int countStop = 0;
+            /// <summary>
+            /// startしてからstopするまでの経過時間を保有する
+            /// </summary>
+            private Stopwatch sw = new Stopwatch();
+            /// <summary>
+            /// 表題とともにインスタンスを生成する
+            /// </summary>
+            /// <param name="title">インスタンスにつける表題</param>
+            public TimeKeeper (String title) {
+                this.title = title;
+            }
+            /// <summary>
+            /// 表題なしでインスタンスを生成する
+            /// </summary>
+            public TimeKeeper () : this(""){}
+
+            /// <summary>
+            /// 経過時間計測を開始する
+            /// </summary>
+            public void start() {
+                sw.Start();
+                countStart++;
+            }
+
+            /// <summary>
+            /// 経過時間計測を終了する
+            /// </summary>
+            public void stop() {
+                sw.Stop();
+                countStop++;
+            }
+
+            /// <summary>
+            /// 計測結果レポートを出力する
+            /// </summary>
+            /// <returns>計測結果を説明する文字列</returns>
+            public string report() {
+
+                float elapsed = sw.ElapsedMilliseconds;
+                System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                sb.Append(string.Format("start={0}", countStart));
+                sb.Append(string.Format(", stop={0}", countStop));
+                sb.Append(string.Format(", totalSec={0:F2}", elapsed/1000));
+                sb.Append(string.Format(", aveSec={0:F2}", (elapsed/1000/countStart)));
+                if (title != "") {
+                    sb.Append(string.Format(", title={0}", title));
+                }
+                if (isTitleChanged) {
+                    sb.Append(", WARNING: title has changed.");
+                }
+                if (countStart != countStop) {
+                    sb.Append(", WARNING: count start/stop not match.");
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
